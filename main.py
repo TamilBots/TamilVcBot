4a93afd8448f15c447c2839ef89bec529c29c848
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-04-16 23:40:52 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from os import environ
+# import logging
+from pyrogram import Client, idle
+
+api_id = int(environ["API_ID"])
+api_hash = environ["API_HASH"]
+session_name = environ["SESSION_NAME"]
+
+plugins = dict(
+    root="plugins",
+    include=[
+        "vc." + environ["PLUGIN"],
+        "ping",
+        "sysinfo"
+    ]
+)
+
+app = Client(session_name, api_id, api_hash, plugins=plugins)
+# logging.basicConfig(level=logging.INFO)
+app.start()
+print('>>> USERBOT STARTED by @TamilBots')
+idle()
+app.stop()
+print('\n>>> USERBOT STOPPED by @TamilBots')
